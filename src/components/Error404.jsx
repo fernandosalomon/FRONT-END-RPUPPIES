f04147cd95a3c70489febdc95391ed3237dd3135
fernandosalomon <fernandosalomon1992@gmail.com>
2024-09-11 18:28:06 -0300

    se corrige redireccionamiento boton volver Error 404

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -1,19 +1,32 @@
-import imagenCartel404 from '../assets/img/error-404-cartel.png';
-import imagen404 from '../assets/img/error-404.png';
+import { Link } from "react-router-dom";
+import imagenCartel404 from "../assets/img/error-404-cartel.png";
+import imagen404 from "../assets/img/error-404.png";
+import { Button } from "react-bootstrap";
 
 const Error404 = () => {
-    return (
-        <>
-            <div className="container-fluid d-flex align-items-center justify-content-center flex-column py-4">
-                <div className="container-fluid d-flex align-items-center justify-content-center">
-                    <h1 className="text-center me-3">Error</h1>
-                    <img src={imagenCartel404} alt="error-404" style={{ width: '60px', height: '60px' }} />
-                </div>
-                <img className="m-4" src={imagen404} alt="error-404" style={{ width: '200px', height: '200px' }} />
-                <button className="btn btnPersonalized1">Volver al inicio</button>
-            </div>
-        </>
-    );
-}
+  return (
+    <>
+      <div className="container-fluid d-flex align-items-center justify-content-center flex-column py-4">
+        <div className="container-fluid d-flex align-items-center justify-content-center">
+          <h1 className="text-center me-3">Error</h1>
+          <img
+            src={imagenCartel404}
+            alt="error-404"
+            style={{ width: "60px", height: "60px" }}
+          />
+        </div>
+        <img
+          className="m-4"
+          src={imagen404}
+          alt="error-404"
+          style={{ width: "200px", height: "200px" }}
+        />
+        <Button as={Link} className="btn btnPersonalized1" to="/">
+          Volver al inicio
+        </Button>
+      </div>
+    </>
+  );
+};
 
 export default Error404;
