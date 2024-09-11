89e79d033fa2078b795a2e34cba2d171e1188a2f
fernandosalomon <fernandosalomon1992@gmail.com>
2024-09-11 17:33:19 -0300

    se corrije error en clientAxios

--- src/helpers/clientAxios.js
+++ src/helpers/clientAxios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const clienteAxios = axios.create({
-  baseURL: `back-end-rpuppies.vercel.app/api`,
+  baseURL: `https://back-end-rpuppies.vercel.app/api`,
 });
 
 export default clienteAxios;
